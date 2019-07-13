b532f52e982071c2e07b93331b2999b61b2a536e
Kai <minecraftworldrunner@gmail.com>
2019-07-13 15:35:56 -0700

    Made constants Made constants printable!

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ class NoEnd(Exception):
     pass
 class NoParens(Exception):
     pass
+class CannotChangeConstant(Exception):
+    pass
 global prev
 varName = []
 varCont = []
+constName=[]
+constCont=[]
+constanted = False
 lsts = []
 mth = False
 line=0
@@ -31,8 +36,10 @@ while br == False:
         vName = il[1]
         vVal = il[3]
         eqeq = il[2]
+        printinglog = "Variable created " + vName + "=" + vVal + "\n"
+        log.write(printinglog)
         if "="  != eqeq:
-            noek = "No Equals LINE " + lin
+            noek = "No Equals LINE " + lin + "\n"
             log.write (noek)
             log.close()
             raise NoEqEq("No Equals",line)
@@ -51,6 +58,9 @@ while br == False:
                     xaf = float(xaf)
                     caf = float(caf)
                     answd = xaf + caf
+                    answdd = str(answd)
+                    mathlog = "Variable created (math)" + il[1] + "=" + answdd + "\n"
+                    log.write(mathlog)
                     varName.append(il[1])
                     varCont.append(answd)
 
@@ -59,21 +69,33 @@ while br == False:
                     answ = il[3] + il[5]
                     varName.append(il[1])
                     varCont.append(answ)
+                    answ = str (answ)
+                    mathlog = "Variable created (math)" + il[1] + "=" + answ + "\n"
+                    log.write(mathlog)
                     mth = True
                 elif op == "-":
                     answ = il[3] - il[5]
                     varName.append(il[1])
                     varCont.append(answ)
+                    answ = str (answ)
+                    mathlog = "Variable created (math)" + il[1] + "=" + answ + "\n"
+                    log.write(mathlog)
                     mth = True
                 elif op == "*":
                     answ = il[3] * il[5]
                     varName.append(il[1])
                     varCont.append(answ)
+                    answ = str (answ)
+                    mathlog = "Variable created (math)" + il[1] + "=" + answ + "\n"
+                    log.write(mathlog)
                     mth = True
                 elif op == "/":
                     answ = il[3] / il[5]
                     varName.append(il[1])
                     varCont.append(answ)
+                    answ = str (answ)
+                    mathlog = "Variable created (math)" + il[1] + "=" + answ + "\n"
+                    log.write(mathlog)
                     mth = True
             if il[1] in varName and mth == False:
                 dvar = varName.index(vName)
@@ -83,45 +105,68 @@ while br == False:
                 global vValue
                 vValue = vVal.split('"')
                 varName.append(vName)
-                varCont.append(vValue[1])
+                if len (vValue) == 1:
+                    varCont.append(vValue[0])
+                else:
+                    varCont.append(vValue[1])
             vValue = vVal.split('"')
             varName.append(vName)
             if len(vValue) == 1:
                 varCont.append(vValue[0])
+                repls = "Variable " + il[1] + " Replaced\n"
+                log.write(repls)
             else:
                 varCont.append(vValue[1])
+                repls = "Variable " + il[1] + " Replaced\n"
+                log.write(repls)
             prev = vVal
     elif "delvar" == il[0]:
-        varToDel = il[1]
+        varToDel = varName.index(il[1])
         varToDel = int(varToDel)
+        deletvar = "Variable Deleted " + il[1] + "\n"
+        log.write(deletvar)
         del varName[varToDel]
         del varCont[varToDel]
     elif "console.println" == il[0]:
-        if '"' in il[1]:
+        num = il[1].split("(")
+        numba = num[1].split(")")
+        if numba[0] in constName:
+            printConst = constName.index(numba[0])
+            print (constCont[printConst])
+            constanted = True
+        if '"' in il[1] and constanted == False:
             content=""
             for x in range(1,len(il)):
                 content=content+il[x]+" "
             cat = content.split('"')
+            printlog = "Printed " + cat[1] + "\n"
+            log.write (printlog)
             print (cat[1])
-        else:
+        if '"' not in il[1] and constanted == False:
             parens = ad[line].split("(")
             parens = str(parens)
             paren = parens.split(")")
             paren = str(paren)
             parn = paren.split("'")
             parnn = parn[3].split('"')
-            if parnn[0] in varName:
+            if parnn[0] in varName and constanted == False:
                 rob = varName.index(parnn[0])
                 bst = varCont[rob]
                 bst = str(bst)
+                varprint = "Printed Var " + bst
+                log.write(varprint)
                 print (bst)
-            else:
-                nopanren = il[1].split(")")
-
-                noopen = nopanren[0].split("(")
-                bart = lsts.index(noopen[1])
-                bart = bart + 1
-                print (lsts[bart])
+            if parnn[0] not in varName:
+                if constanted == False:
+                    print ("insli")
+                    nopanren = il[1].split(")")
+                    noopen = nopanren[0].split("(")
+                    bart = lsts.index(noopen[1])
+                    bart = bart + 1
+                    listprint = "Printed List Entry" + lsts[bart] + "\n"
+                    print (lsts[bart])
+                else:
+                    pass
     elif "if" == il[0]:
         eckeck = il[2]
         if eckeck != "=":
@@ -137,6 +182,8 @@ while br == False:
             if v2 not in varName:
                 v2 = int(v2)
                 if v1 == v2:
+                    correctif = "If statement success " + v1 + " = " + v2 + "\n"
+                    log.write(correctif)
                     global intulhere
                     intulhere = 0
                     for x in range(line+1,len(ad)):
@@ -151,13 +198,15 @@ while br == False:
                 stick = varName.index(v2)
                 v2 = varCont[stick]
                 if v1 != v2:
+                    incorrectif = "If statement failed " + v1 + " != " + v2 + "\n"
+                    log.write(incorrectif)
                     global untilhere
                     untilhere=0
                     for x in range(line+1,len(ad)):
                         if ad[x]=="}":
                             untilhere=x
                     if untilhere==0:
-                        noendof = "No End To If Statement "+lin
+                        noendof = "No End To If Statement "+lin + "\n"
                         log.write(noendof)
                         log.close()
                         raise NoEnd("No end to IF",line)
@@ -166,33 +215,43 @@ while br == False:
         nameovar = il[1]
         ract = ad[line].split('"')
         response = input(ract[1])
+        loginput = "Input asked, Response: " + response + "\n"
+        log.write (loginput)
         varName.append(nameovar)
         varCont.append(response)
         response = ""
     elif "integ" == il[0]:
         integify = varName.index(il[1])
         varCont[integify] = int(varCont[integify])
+        integlog = "var " + il[1] + " changed type to integ\n"
     elif "strin" == il[0]:
         strinify = varName.index(il[1])
         varCont[strinify] = str(varCont[strinify])
+        strinlog = 0
     elif "bool" == il[0]:
         boolify = varName.index(il[1])
         varCont[boolify] = bool(varCont[boolify])
+        boolog = 0
     elif "flt" == il[0]:
         floatify = varName.index(il[1])
         varCont[floatify] = float(varCont[floatify])
+        floatlog = 0
     elif "type" == il[0]:
         tip = varName.index(il[2])
         asgn = il[1]
         varName.append(asgn)
         if type(varCont[tip]) == str:
             varCont.append("strin")
+            typestrin
         elif type(varCont[tip]) == int:
             varCont.append("integ")
+            typeinteg
         elif type(varCont[tip]) == float:
             varCont.append("flt")
+            typeflt
         elif type(varCont[tip]) == bool:
             varCont.append("bool")
+            typebool
     elif "lst" == il[0]:
         lsts.append(il[1])
         lsts.append([])
@@ -255,7 +314,19 @@ while br == False:
         else:
             varName.append(varToAssign)
             varCont.append(lenny)
-
+    elif "const" == il[0]:
+        if il[1] in constName:
+            raise CannotChangeConstant ("Cannot change value of constant")
+        else:
+            constAddName = il[1]
+            constAddContent = il[3]
+            equalss = il[2]
+            if equalss != '=':
+                log.write("No Equals for Constant")
+                raise NoEqEq ("No Equals for Constant")
+            else:
+                constName.append(constAddName)
+                constCont.append(constAddContent)
 
 
     if line + 1 == len(ad):
